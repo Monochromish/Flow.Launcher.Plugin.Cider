fbce768e209c0349c5f487de29a72564824f0c56
Monochromish <79590499+Monochromish@users.noreply.github.com>
2022-10-20 03:41:10 +1100

    Catch error for node not installed

--- node.bat
+++ node.bat
@@ -1,3 +1,13 @@
 @echo off
 SET plugin_dir=%~dp0%
-node "%plugin_dir%/main.js" %*
+SET icon=%plugin_dir%Assets\app.png
+SET "icon=%icon:\=/%"
+SET title="Error: Node.js is not installed"
+SET subtitle="To install and use this plugin, you need to install node.js from https://nodejs.org"
+
+where /q node
+IF ERRORLEVEL 1 (
+    echo {"result": [{"Title": %title%, "Subtitle": %subtitle%, "IcoPath": "%icon%"}]}
+) ELSE (
+    node "%plugin_dir%/main.js" %*
+)
